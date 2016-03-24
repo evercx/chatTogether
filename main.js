2020d9ad987ffcc72af77f7e4b544cae41e10948
evercx <306534137@qq.com>
2016-03-24 10:34:32 +0800

    version 1.0.0

---
+++ main.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var hbs = require('hbs');
+var app=express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var onlineUsrs=[];
+//监听socket连接
+io.on('connection',function(socket){
+	var client={
+		usrname:'',
+		time:'',
+		msg:'',
+		online:false
+	}
+	socket.on('registername', function (data) {
+		if(!client.usrname){	//第一次连接
+			if(onlineUsrs.indexOf(data)!== -1){
+				socket.emit('nameerror',{msg:"昵称重复 请重试"});
+			}else{
+				onlineUsrs.push(data);
+				client.usrname=data;
+				client.msg="用户“"+client.usrname+"”上线了";
+				client.time=getTime();
+				client.online=true;
+				//广播系统通知
+				socket.broadcast.emit('system',client);
+				socket.emit('system',client);
+				//广播在线用户表
+				//socket.broadcast.emit('users',{users:onlineUsrs});
+				socket.emit('users',{users:onlineUsrs});
+			}
+		}
+	});
+	socket.on('message', function (data) {
+		client.msg=data;
+		client.time=getTime();
+		socket.broadcast.emit('msg',client);
+		socket.emit('msg',client);
+	});
+    socket.on('disconnect', function () {
+    	if(client.usrname!==''){
+			client.msg="用户“"+client.usrname+"”下线了";
+			client.time=getTime();
+			if(onlineUsrs.indexOf(client.usrname)>-1){
+				console.log(onlineUsrs.indexOf(client.usrname));
+				onlineUsrs.splice(onlineUsrs.indexOf(client.usrname));//下线后取消保存该用户名
+			}
+			console.log(onlineUsrs);
+			client.online=false;
+			socket.broadcast.emit('system',client);
+			client.usrname='';
+    	}
+			
+    });
+
+});
+
+app.set('port', process.env.PORT || 30653);//设置端口号
+app.set('view engine','html');
+app.engine('html',hbs.__express);//设置模版引擎
+app.use(express.static('public'));//指定静态文件夹为public目录
+
+//设置中间件，运行Html上Javascript脚本进行跨域访问
+app.use(function(req,res,next){
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
+  next();
+  //console.log("use");
+
+});
+
+app.get('/',function(req,res){
+	res.render('chat');
+	//res.render('index');
+
+});
+app.post('/',function(req,res){
+	console.log(req.a);
+	if(onlineUsrs.indexOf(req.nickname)!== -1){
+		res.json({err:"昵称重复 请重试"});
+	}else{
+		onlineUsrs.push(req.nickname);
+		console.log(req.nickname);
+		console.log(onlineUsrs);
+		res.sendfile('views/chatroom.html');
+	}	
+});
+
+
+server.listen(app.get('port'),function(){
+	console.log('this server is listening on port:'+app.get('port'));
+});
+
+
+var getTime=function(){
+  var date = new Date();
+  return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+}
---
+++ public/js/chat.js
@@ -0,0 +1,118 @@
+
+'use strict';
+var socket = io.connect(ip);
+var myName='';
+
+function showModal(){
+	$('#alertNickName').modal({backdrop:'static', keyboard: false});
+	deleteBackFade();
+}
+function deleteBackFade(){
+	var t=document.getElementsByClassName("modal-backdrop fade in");
+	for(var i=0;i<t.length-1;i++)    
+	  {  
+	     t[i].parentNode.removeChild(t[i]); 
+	  }
+}
+
+
+$(function(){
+	 showModal();
+});
+
+//提交昵称
+$("#submit").click(function(){
+	var nickname = $("#nickname").val();
+	if(nickname!==''){
+		socket.emit('registername',nickname);
+		myName=nickname;
+		$('#alertNickName').modal('hide');
+		deleteBackFade();
+	}
+});
+
+
+
+socket.on('system', function (data) {
+    getSystemMsgDIV(data);
+    //当用户上线了 添加到在线用户表中
+    if(data.online){
+		var $option='<option value= "'+data.usrname+'">'+data.usrname+'</option>'
+		$('.usrlist').append($option);
+    }
+    //当用户下线了 从在线用户表中删除
+    if(!data.online){
+		
+		$('option[value='+data.usrname+']').remove();
+    }
+
+});
+
+socket.on('msg', function (data) {
+	console.log(data);
+    if(data.usrname===myName){
+    	getMyDIV(data);
+    }else{
+    	getOthersDIV(data);
+    }
+});
+
+socket.on('users',function(data){
+	console.log(data.users);
+
+	$('.usrlist').empty();
+	$('.usrlist').append('<option   value= "title" style="font-weight:bold">在线用户：</option> ');
+	for(var i in data.users){
+		var $option='<option value= "'+data.users[i]+'">'+data.users[i]+'</option>'
+		$('.usrlist').append($option);
+	}
+});
+
+
+socket.on('nameerror', function (data) {
+	if(data){
+	    console.log(data.msg);
+	    alert(data.msg);
+	    showModal();
+	}
+});
+
+
+$("#launch").click(function () {
+	if($("#msg").val()!==''){
+		socket.send($("#msg").val());
+		$("#msg").val('');		
+	}
+
+});
+
+$("#clear").click(function(){
+	$("#chatbg").empty();
+});
+
+function getOthersDIV(json){
+	var $div='';
+	$div='<div class="alert alert-info others">'+json.usrname+' @ '+json.time+'：</br>'+json.msg+'</div>'
+	$("#chatbg").append($div);
+}
+function getMyDIV(json){
+	console.log(json);
+	var $div="";
+	$div='<div class="alert alert-success me">我 @ '+json.time+'：</br>'+json.msg+'</div>';
+	$("#chatbg").append($div);
+}
+
+function getSystemMsgDIV(json){
+	var $div="";
+	//$div='<div ><p class="text-danger">'+json.msg+'</p></div>';
+	$div='<option   value= "'+json.time+'">system @ '+json.time+'： '+json.msg+'</option>'
+	console.log($div);
+	$(".systemmsg").append($div);
+}
+
+//通过“回车”提交聊天信息
+/*$("#msg").keydown(function(e) {
+    if (e.keyCode === 13) {
+		$("#launch").click();
+    }
+});*/
